ad084aec39e36835452739b7c0b3734531ebce1c
Aman Prasad <amanprasad1072@gmail.com>
2022-07-28 14:12:16 +0530

    fix

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ app.use(require('./controller/routes.js'));
 
 const port = process.env.PORT || 8000;
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(port, function(){
+    console.log("Express server listening on port %d in %s mode", port);
   });
